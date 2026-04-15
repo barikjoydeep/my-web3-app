2db46ecb07b97546d55db0bb4ddc7fbe818fa75e
JOYDEEP BARIK <141179024+barikjoydeep@users.noreply.github.com>
2026-04-16 00:52:06 +0530

    Add wallet connection functionality

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const contractAddress = "0x7E44dCADE31707393B0E2c3bcFB052ddB3bECfE6"
+async function connectWallet() {
+
+  if (!window.ethereum) {
+    alert("Install MetaMask");
+    return;
+  }
+
+  const accounts = await window.ethereum.request({
+    method: "eth_requestAccounts",
+  });
+
+  document.getElementById("status").innerText =
+    "Connected: " + accounts[0];
+}
